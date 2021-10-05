f1d450cc2672df25a34bf84f29f4e50583a42674
Samuel De Paz <sdepaz@eseirb-mateafr>
2021-10-05 15:43:04 +0200

    add functions

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -3,12 +3,72 @@ package com.example.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String number1 = "";
+    String number2 = "";
+    String sign="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void RefreshNumber(TextView t){
+        t.setText(number1 + sign + number2);
+    }
+
+    public void OnEqualPress(View v){
+        if (sign.length() != 0){
+            int n1 = Integer.parseInt(number1);
+            int n2 = Integer.parseInt(number2);
+            int res = 0;
+            if (sign.equals("+")){
+                res = n1+n2;
+            }
+            if (sign.equals("-")){
+                res = n1-n2;
+            }
+            if (sign.equals("*")){
+                res = n1*n2;
+            }
+            if (sign.equals("/")){
+                res = n1/n2;
+            }
+            number1 = "";
+            number2 = String.valueOf(res);
+            sign = "";
+        }
+        RefreshNumber(findViewById(R.id.textView));
+    }
+
+    public void OnNumberPress(View v){
+        Button b = (Button)v;
+        number2 += b.getText().toString();
+        RefreshNumber(findViewById(R.id.textView));
+    }
+
+    public void OnSignPress(View v){
+        if (sign.length() == 0){
+            OnEqualPress(v);
+        }
+        Button b = (Button)v;
+        sign = b.getText().toString();
+        number1 = number2;
+        number2 = "";
+        RefreshNumber(findViewById(R.id.textView));
+
+    }
+
+    public void OnCPress(View v){
+        number1 = "";
+        number2 = "";
+        sign= "";
+        RefreshNumber(findViewById(R.id.textView));
     }
 }
